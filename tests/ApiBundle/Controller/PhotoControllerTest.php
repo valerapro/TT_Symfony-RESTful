ddb3a2c6064f33b1fa3fa06cec99047bb430b3ac
Valery <valerij.budko@gmail.com>
2017-02-08 11:23:18 +0200

    1 test done

--- tests/ApiBundle/Controller/PhotoControllerTest.php
+++ tests/ApiBundle/Controller/PhotoControllerTest.php
@@ -8,6 +8,79 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class PhotoControllerTest extends WebTestCase
 {
+
+    /**
+     * Base rules for test methods
+     *
+     * @param $client
+     * @param $crawler
+     */
+    public function basicRulesTest($client, $crawler)
+    {
+        // Assert that the response status code is 2xx
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        // Assert that the response status code is 200
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $client->getResponse()->getStatusCode()
+        );
+        // Assert that the "Content-Type" header is "application/json"
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            )
+        );
+    }
+
+    /**
+     * Create temp tag
+     *
+     * @return integer
+     */
+    public function createTag()
+    {
+        $clientPrep = static::createClient();
+        $crawlerPred = $clientPrep->request('POST', '/api/v1/tags', ['title' => 'Test_' . rand()]);
+        $resultRequest = $clientPrep->getResponse()->getContent();
+        $resultRequestJson = json_decode($resultRequest, true);
+
+        return $resultRequestJson['id'];
+    }
+
+    /**
+     * Delete tag
+     *
+     * @param $tagId
+     */
+    public function delTag($tagId)
+    {
+        $clientPrep = static::createClient();
+        $clientPrep->request('DELETE', '/api/v1/tags/' . $tagId);
+    }
+
+    /**
+     * Create image on server
+     *
+     * @return string
+     */
+    public function createPhoto(){
+        $testImagePath = __DIR__ . '/../../../web/images/';
+        $testImage = 'test.jpg';
+        $testImageFullPath = $testImagePath . $testImage;
+
+        if (!file_exists($testImageFullPath)) {
+            var_dump('Test image is not reachable');
+            var_dump($testImageFullPath);
+        }
+        $newName = rand() . ".jpg";
+        $newTestImage = $testImagePath . $newName;
+        exec("cp $testImageFullPath $newTestImage");
+
+        return $newTestImage;
+    }
+
     /**
      * GET /api/v1/photos
      * Info: get list all photo with no pagination
@@ -17,21 +90,7 @@ class PhotoControllerTest extends WebTestCase
         $client = static::createClient();
         $crawler = $client->request('GET', '/api/v1/photos');
 
-        // Assert that the response status code is 2xx
-        $this->assertTrue($client->getResponse()->isSuccessful());
-
-        // Assert that the response status code is 200
-        $this->assertEquals(
-            Response::HTTP_OK,
-            $client->getResponse()->getStatusCode()
-        );
-        // Assert that the "Content-Type" header is "application/json"
-        $this->assertTrue(
-            $client->getResponse()->headers->contains(
-                'Content-Type',
-                'application/json'
-            )
-        );
+        $this->basicRulesTest($client, $crawler);
     }
 
     /**
@@ -40,17 +99,18 @@ class PhotoControllerTest extends WebTestCase
      */
     public function testNewPhoto()
     {
-        $testImagePath = __DIR__ . '/../../../web/images/' ;
-        $testImage = 'test.jpg' ;
-        $testImageFullPath = $testImagePath . $testImage;
-
-        if (!file_exists($testImageFullPath)) {
-            var_dump('Test image is not reachable');
-            var_dump($testImageFullPath);
-        }
-        $newName = rand(). ".jpg";
-        $newTestImage = $testImagePath . $newName;
-        exec("cp $testImageFullPath $newTestImage");
+        /**
+         * Generate temp tags.
+         * Separated by comma
+         */
+        $tags = $this->createTag() . ',' . $this->createTag();
+        /**
+         * Image with path
+         */
+        $newTestImage = $this->createPhoto();
+        /**
+         * Testing
+         */
         $photo = new UploadedFile(
             $newTestImage,
             'test.jpg',
@@ -59,28 +119,21 @@ class PhotoControllerTest extends WebTestCase
         );
 
         $client = static::createClient();
-        $client->request('POST', '/api/v1/photos',
-            ['tags' =>  ''],
+        $crawler = $client->request('POST', '/api/v1/photos',
+            ['tags' => $tags],
             ['file' => $photo]
-            );
-
-        // Assert that the response status code is 2xx
-        $this->assertTrue($client->getResponse()->isSuccessful());
-
-        // Assert that the response status code is 200
-        $this->assertEquals(
-            Response::HTTP_OK,
-            $client->getResponse()->getStatusCode()
         );
-        // Assert that the "Content-Type" header is "application/json"
-        $this->assertTrue(
-            $client->getResponse()->headers->contains(
-                'Content-Type',
-                'application/json'
-            )
-        );
+
+        $this->basicRulesTest($client, $crawler);
+
+        /**
+         * Del tags
+         */
+        $tagsArr = explode(',', $tags);
+        foreach ($tagsArr as $value){
+            $this->delTag($value);
+        }
     }
-
 
 
 }
